61005f0b038ee80c4d44b6c60f506fe0ed26c68f
nirmal1str <nirmalseshasai24@gmail.com>
2025-09-19 06:35:20 +0530

    Update API Ul for Vercel deployement.

--- frontend/src/api/apiService.js
+++ frontend/src/api/apiService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:5000/api';
+// Change this to your new Render backend URL
+const API_BASE_URL = 'https://backend-mggs.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
